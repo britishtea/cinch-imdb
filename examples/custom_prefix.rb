26104d4a5b15f09d2e4333b93963c63782399395
Paul Brickfeld <jip@MacBook-Pro-van-Jip.local>
2011-10-24 15:11:59 +0200

    New custom prefix example
    Despite this being a standard feature of Cinch, it's nice to point out.

---
+++ examples/custom_prefix.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require "cinch"
+require "cinch/plugins/imdb"
+
+class Cinch::Plugins::Imdb
+  prefix "!"
+end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick = "testsie"
+    c.port = 6667
+    c.server = "irc.what-network.net"
+    c.channels = ["#film"]
+    
+    c.plugins.plugins = [Cinch::Plugins::Imdb]
+  end
+end
+
+bot.start
